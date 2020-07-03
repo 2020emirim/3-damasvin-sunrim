2dedd9cc20bf57929c5f33711bc220cc222acccf
tjgpfla020 <tjgpfla020@gmail.com>
2020-07-03 14:55:32 +0900

    templates/views initial

--- menu/views.py
+++ menu/views.py
@@ -9,25 +9,27 @@ class DrinkListView(ListView):
     model = Drink
     # paginate_by = 3
 
-class CoffeekCreateView(CreateView):
+class CoffeeCreateView(CreateView):
     model = Coffee
     fields = '__all__'  # ['category','name','price','image']
-    template_name = 'drink_create.html'
+    template_name = 'menu/drink_create.html'
     success_url = reverse_lazy('menu:list')
+    initial = {'category':'Coffee'}
 
-class BubblteakCreateView(CreateView):
+class BubbleteaCreateView(CreateView):
     model = Bubbletea
     fields = '__all__'  # ['category','name','price','image']
-    template_name = 'drink_create.html'
+    template_name = 'menu/drink_create.html'
     success_url = reverse_lazy('menu:list')
+    initial = {'category': 'Bubbletea'}
 
 
 class DrinkUpdateView(UpdateView):
     model = Drink
     fields = '__all__' # <form>가 있을 때 쓴다
-    template_name_suffix = '_update'    #bookmark_update.html
+    template_name_suffix = '_update'
     success_url = reverse_lazy('menu:list')
 
-class DrinkkDeleteView(DeleteView):
+class DrinkDeleteView(DeleteView):
     model = Drink
     success_url = reverse_lazy('menu:list')
