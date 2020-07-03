ebb8ef855736029c815dc840bd81ba4cf5c00f20
tjgpfla020 <tjgpfla020@gmail.com>
2020-07-03 15:43:56 +0900

    order/views orderListView

--- order/views.py
+++ order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from order.models import Order
+
+
+class OrderListView(ListView):
+    model = Order
